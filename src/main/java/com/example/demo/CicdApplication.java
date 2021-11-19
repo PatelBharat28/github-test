4c3234c244078b6174ddc7989c49109d255d9c98
patelBharat28 <bharat2839@gmail.com>
2021-11-19 18:51:09 +0530

    first commit

---
+++ src/main/java/com/example/demo/CicdApplication.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdApplication {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to git hub deployment";
+	}
+	
+	//echo "# github-test" >> README.md
+	//git init
+	//git add README.md
+	//git commit -m "first commit"
+	//git branch -M main
+	//git remote add origin https://github.com/PatelBharat28/github-test.git
+	//git push -u origin main
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
